bcf87569fea7af605a6845673d1cb3654baea595
Jongseo Lee <jongseo12345@gmail.com>
2024-11-05 11:17:49 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241105/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+#include <utility>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+int dp[1001] = { 0, };
+int vs[3000009] = { 0, };
+int main() {
+    dp[1] = 2;
+    dp[2] = 7;
+    
+    int n;
+    cin >> n;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1]*2) % 10007 + (dp[i - 2]*4)% 10007;
+        dp[i] %= 10007;
+    }
+    
+    cout << dp[n];
+    return 0;
+}
