2e2b927bebbbfdf2154dac3d3924e0baadcc0787
Jongseo Lee <jongseo12345@gmail.com>
2025-04-05 13:45:39 +0900

    Title: 바이러스 검사, Time: 359ms, Memory: 4MB, Status: Wrong Answer - Codetree

---
+++ 250405/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int>p;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        p.push_back(temp);
+    }
+
+    int l, s;
+    cin>>l>>s;
+
+    int res = 0;
+
+    for(auto pp:p){
+        res+=1;
+        pp -= l;
+        if (pp>0){
+            res+= pp/s;
+            if (pp%s != 0){
+                res+=1;
+            }
+        }
+    }
+    cout<<res;
+    return 0;
+}
